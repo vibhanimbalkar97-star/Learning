51bd52e5a88bec90ec3cb45b991e1d6b807076d4
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-07-10 20:51:44 +0530

    strings methods done

--- React/Javascript/Practice/src/components/ArrayMethods/MapExample.jsx
+++ React/Javascript/Practice/src/components/ArrayMethods/MapExample.jsx
@@ -1,17 +1,7 @@
 const MapExample = () => {
-  const a = [
-    { name: "John", age: 30 },
-    { name: "Ram", age: 20 },
-    { name: "Amit", age: 25 },
-  ];
-
-    const res = a.map((item) => (item.name)).join("-")
-
-  console.log(res);
-
-  return <div>
-    
-  </div>;
+ 
+  
+  return <div></div>;
 };
 
 export default MapExample;
