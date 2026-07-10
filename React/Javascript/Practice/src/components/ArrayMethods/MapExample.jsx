080f319429980ae7b75aee53e6e038fabf3b5946
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-07-10 15:55:26 +0530

    Array methods done

--- React/Javascript/Practice/src/components/ArrayMethods/MapExample.jsx
+++ React/Javascript/Practice/src/components/ArrayMethods/MapExample.jsx
@@ -1,15 +1,16 @@
 const MapExample = () => {
-  const n =  [
+  const a = [
     { name: "John", age: 30 },
     { name: "Ram", age: 20 },
     { name: "Amit", age: 25 },
-  ];;
+  ];
 
- const res = n.splice(1,1)
+    const res = a.map((item) => (item.name)).join("-")
 
   console.log(res);
 
   return <div>
+    
   </div>;
 };
 
