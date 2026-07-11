bee30a4b2935138ee2ee47731e6599d88b852471
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-07-11 05:45:20 +0530

    object methods done

--- React/Javascript/Practice/src/components/ArrayMethods/MapExample.jsx
+++ React/Javascript/Practice/src/components/ArrayMethods/MapExample.jsx
@@ -1,5 +1,24 @@
 const MapExample = () => {
- 
+ const a = {
+name:"John",
+age:20
+};
+
+  // const res = Object.keys(a) //['name', 'age']
+  // const res = Object.values(a) //['John', 20]
+  // const res = Object.entries(a) //[Array(2), Array(2)]
+  // const res = Object.keys(a) //['name', 'age']
+  // const res = Object.keys(a) //['name', 'age']
+  // const res = Object.keys(a) //['name', 'age']
+
+  // spread operator
+  // const res = {...a}
+  // res.age = 40 //{name: 'John', age: 40}
+
+  // destructing
+  // const {name, age} = a
+
+  // console.log(name, age)//John 20
   
   return <div></div>;
 };
